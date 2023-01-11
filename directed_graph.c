0f232b085f588fab624826fd3ee8f4c6da0cce58
nitayGitHub <nitaylevi1998@gmail.com>
2023-01-11 22:10:20 +0200

    upd

--- directed_graph.c
+++ directed_graph.c
@@ -1,35 +1,52 @@
 #include "graph.h"
 #include <stdio.h>
+#include <ctype.h>
 
 void build_graph_cmd(pnode *head)
 {
     pnode temp = *head;
     int num_of_nodes = 0, i = 0;
-    char input = 'c';
+    char input = 'n';
     scanf("%d", &num_of_nodes);
-
-    
-    scanf("%c", &input);
-    for (int i = 0; i < num_of_nodes; i++)
+    if (head != NULL)
+    {
+        free(head);
+    }
+    // allocate storage for the graph data structure
+    head = malloc(sizeof(pnode));
+    if (head == NULL)
+    {
+        printf("Error: malloc failed");
+        return;
+    }
+    // initialize head pointer for all vertices
+    for (i = 0; i < num_of_nodes; i++)
+    {
+        head[i] = malloc(sizeof(node));
+        if (head[i] == NULL)
+        {
+            printf("Error: malloc failed");
+            return;
+        }
+        head[i]->next = NULL;
+    }
+    // add edges to the directed graph one by one
+    node *new_node = malloc(sizeof(node));
+    while (!isalph(input) || input == 'n')
     {
         scanf("%c", &input);
-        while(input != 'n')
-        switch (input)
+        if (input == 'n')
         {
-        case 'A':
-            build_graph_cmd(head);
-            break;
-
-        case 'B':
-
-            break;
-
-        case 'C':
-
-            break;
-
-        default:
-            break;
+            scanf("%c", &input);
+            new_node->node_num = atoi(input);
+        }
+        else
+        {
+            edge *e = malloc(sizeof(edge));
+            e->endpoint = head + new_node->node_num;
+            scanf("%c", &input);
+            e->weight = atoi(input);
         }
     }
+    free(new_node);
 }
