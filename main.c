af9d0f939477ace70f67a5d7550a96e2e6bfad20
nitayGitHub <nitaylevi1998@gmail.com>
2023-01-11 15:39:31 +0200

    goodluck

---
+++ main.c
@@ -0,0 +1,30 @@
+#include "graph.h"
+#include <stdio.h>
+
+int main()
+{
+    int mat[10][10];
+    char input = 'a';
+    while (input != 'D')
+    {
+        scanf("%c", &input);
+        switch (input)
+        {
+        case 'A':
+            createMat(mat);
+            break;
+
+        case 'B':
+            isPath(mat);
+            break;
+
+        case 'C':
+            printf("%d\n", shortestP(mat));
+            break;
+
+        default:
+            break;
+        }
+    }
+    return 0;
+}
